f5164b38b8b7f6b5f98a11e176dca98145ca67a4
Sergio <sergioottati@hotmail.com>
2023-03-02 18:33:50 -0300

    Seguimos modificando

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entrie) =>
+    entrie.isIntersecting
+      ? entrie.target.classList.add("tecno-visible")
+      : entrie.target.classList.remove("tecno-visible")
+  );
+});
+
+observer.observe(document.querySelector("header"));
+observer.observe(document.querySelector(".tecno"));
+observer.observe(document.querySelector(".navBar"));
+observer.observe(document.querySelector(".miProfile "));
