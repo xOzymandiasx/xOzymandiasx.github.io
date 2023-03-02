54741f96b4fffed738cdab3ed4ca1d796245e22b
Sergio <sergioottati@hotmail.com>
2023-03-02 20:43:44 -0300

    Terminando

--- js/script.js
+++ js/script.js
@@ -9,4 +9,7 @@ const observer = new IntersectionObserver((entries) => {
 observer.observe(document.querySelector("header"));
 observer.observe(document.querySelector(".tecno"));
 observer.observe(document.querySelector(".navBar"));
-observer.observe(document.querySelector(".miProfile "));
+observer.observe(document.querySelector(".miProfile"));
+observer.observe(document.querySelector(".cursos"));
+observer.observe(document.querySelector(".projects"));
+observer.observe(document.querySelector(".socials-div"));
